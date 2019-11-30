a7f4edf63173d36bf1a419058d25b80b52b0b02b
Jonatan Arrocha Kang <erickangweisz@gmail.com>
2019-11-30 10:19:14 +0000

    generate task-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule
